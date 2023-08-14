ad48a06d9d100e81549fe00fa748e08bd2d2ebad
KINjoohyun <gurados@gmail.com>
2023-08-14 16:55:39 +0900

    SceneLobby  - test code 변경.
    SceneLobbyResourceList.csv
     - test용 그래픽 추가.
    
    Enemy
     - OneShot 함수 구현.
     - FiveWayShot 함수 구현.
     - SixWayDie 함수 구현.

--- Enter the Gungeon/GameObjects/Enemy.cpp
+++ Enter the Gungeon/GameObjects/Enemy.cpp
@@ -1,15 +1,19 @@
 #include "stdafx.h"
 #include "Enemy.h"
 #include "Player.h"
+#include "EnemyBullet.h"
+#include "SceneGame.h"
+
+#include "SceneLobby.h" //test
 
 Enemy::Enemy(EnemyTypes type, const std::string& textureId, const std::string& n)
 	:SpriteGo(textureId, n), type(type)
 {
-	way.push_back({0.f, -1.f});
-	way.push_back(Utils::Normalize({1.f, -1.f}));
-	way.push_back({1.f, 0.f});
-	way.push_back(Utils::Normalize({1.f, 1.f}));
-	way.push_back({0.f, 1.f});
+	way.push_back({ 0.f, -1.f }); // Up
+	way.push_back(Utils::Normalize({ 1.f, -1.f })); // LeftUp
+	way.push_back({ 1.f, 0.f }); // Left
+	way.push_back(Utils::Normalize({ 1.f, 1.f })); // LeftDown
+	way.push_back({ 0.f, 1.f }); // Down
 }
 
 Enemy::~Enemy()
@@ -25,17 +29,38 @@ void Enemy::Init()
 	case EnemyTypes::BulletKin:
 		name = "BulletKin";
 		isHanded = true;
+		IfShoot = [&](sf::Vector2f dir, float speed)
+		{
+			OneShot(dir, speed);
+		};
+		maxHp = 15.f; // table 사용
 		break;
 	case EnemyTypes::KeyBulletKin:
 		name = "KeyBulletKin";
+		// Runaway 함수
+		maxHp = 15.f;
 		break;
 	case EnemyTypes::ShotgunKinRed:
 		name = "ShotgunKinRed";
 		isHanded = true;
+		IfShoot = [&](sf::Vector2f dir, float speed)
+		{
+			FiveWayShot(dir, speed);
+		};
+		IfDie = [&](sf::Vector2f dir)
+		{
+			SixWayDie(dir, speed, 20); // table 사용
+		};
+		maxHp = 30.f; // table 사용
 		break;
 	case EnemyTypes::ShotgunKinBlue:
 		name = "ShotgunKinBlue";
 		isHanded = true;
+		IfDie = [&](sf::Vector2f dir)
+		{
+			SixWayDie(dir, speed, 33); // table 사용
+		};
+		maxHp = 40.f; // table 사용
 		break;
 	default:
 		std::cerr << "ERROR: Not Exist EnemyTypes (Enemy Init())" << std::endl;
@@ -94,22 +119,60 @@ void Enemy::Reset()
 
 	hp = maxHp;
 	isAlive = true;
+	attackTimer = attackInterval;
 }
 
 void Enemy::Update(float dt)
 {
 	animation.Update(dt);
 
-	if (!isAlive) return;
+	if (!isAlive || player == nullptr) return;
 
 	direction = Utils::Normalize(player->GetPosition() - position);
+	if (direction != sf::Vector2f{ 0.f, 0.f }) prevDir = direction;
 	float distance = Utils::Distance(player->GetPosition(), position);
+	sf::Vector2f min = WhereWay(direction);
 
-	if (attackRange > distance) //벽을 만났을 때 사격할 수 있게 재설정 필요
+	if (attackRange > distance)
 	{
 		direction = { 0.f, 0.f };
+
+		attackTimer += dt;
+		if (attackTimer >= attackInterval)
+		{
+			attackTimer = 0.f;
+			if (IfShoot != nullptr)
+			{
+				IfShoot(prevDir, speed); //총알 속도 지정 필요
+			}
+
+			// Animation
+			if (min == way[0])
+			{
+				animation.Play("AttackUp");
+			}
+			else if (min == way[1])
+			{
+				animation.Play("AttackLeftUp");
+			}
+			else if (min == way[2])
+			{
+				animation.Play("AttackLeft");
+			}
+			else if (min == way[3])
+			{
+				animation.Play("AttackLeftDown");
+			}
+			else if (min == way[4])
+			{
+				animation.Play("AttackDown");
+			}
+		}
 	}
-	SetPosition(position + direction * speed * dt);
+	else
+	{
+		SetPosition(position + direction * speed * dt);
+	}
 	SetFlipX(direction.x > 0.f);
 
 	if (player->sprite.getGlobalBounds().intersects(sprite.getGlobalBounds())) // Collider 충돌로 변경 요구
@@ -124,7 +187,6 @@ void Enemy::Update(float dt)
 		}
 	}
 	
-	sf::Vector2f min = WhereWay(direction);
 
 	// Animation
 	if (direction.x != 0.f || direction.y != 0.f)
@@ -217,17 +279,17 @@ void Enemy::SetFlipX(bool flip)
 
 sf::Vector2f Enemy::WhereWay(sf::Vector2f dir)
 {
-	sf::Vector2f min;
+	sf::Vector2f result;
 	float minf = 1.f;
 	for (auto it : way)
 	{
-		if (float result = Utils::Distance({ abs(dir.x), dir.y }, it) < minf)
+		if (float diff = Utils::Distance({ abs(dir.x), dir.y }, it) < minf)
 		{
-			minf = result;
-			min = it;
+			minf = diff;
+			result = it;
 		}
 	}
-	return min;
+	return result;
 }
 
 void Enemy::SetPlayer(Player* player)
@@ -235,18 +297,19 @@ void Enemy::SetPlayer(Player* player)
 	this->player = player;
 }
 
-void Enemy::SetEnemy(float speed, float maxHp, float attackRange, bool superarmor)
+void Enemy::SetEnemy(float speed, float maxHp, float attackRange, float attackInterval, bool superarmor)
 {
 	this->speed = speed;
 	this->maxHp = maxHp;
 	this->attackRange = attackRange;
+	this->attackInterval = attackInterval;
 	this->superarmor = superarmor;
 }
 
-void Enemy::OnDamage(const float& damage, const sf::Vector2f& dir, const float& knockback)
+void Enemy::OnDamage(const float& damage, sf::Vector2f dir, const float& knockback)
 {
 	if (!superarmor) SetPosition(position + dir * knockback);
-	sf::Vector2f min = WhereWay(dir);
+	dir = WhereWay(dir);
 	SetFlipX(dir.x > 0.f);
 
 	if (IfHit != nullptr)
@@ -261,33 +324,33 @@ void Enemy::OnDamage(const float& damage, const sf::Vector2f& dir, const float& 
 		{
 			if (IfDie != nullptr)
 			{
-				IfDie();
+				IfDie(dir);
 			}
 			else
 			{
-				OnDie(min);
+				OnDie(dir);
 			}
 		}
 	}
 
 	// Animation
-	if (min == way[0])
+	if (dir == way[0])
 	{
 		animation.Play("HitUp");
 	}
-	else if (min == way[1])
+	else if (dir == way[1])
 	{
 		animation.Play("HitLeftUp");
 	}
-	else if (min == way[2])
+	else if (dir == way[2])
 	{
 		animation.Play("HitLeft");
 	}
-	else if (min == way[3])
+	else if (dir == way[3])
 	{
 		animation.Play("HitLeftDown");
 	}
-	else if (min == way[4])
+	else if (dir == way[4])
 	{
 		animation.Play("HitDown");
 	}
@@ -299,25 +362,26 @@ void Enemy::OnBump()
 	//player에게 피해를 주는 함수
 }
 
-void Enemy::OnDie(const sf::Vector2f& dir)
+void Enemy::OnDie(const sf::Vector2f& look)
 {
-	if (dir == way[0])
+	// Animation
+	if (look == way[0])
 	{
 		animation.Play("DieUp");
 	}
-	else if (dir == way[1])
+	else if (look == way[1])
 	{
 		animation.Play("DieLeftUp");
 	}
-	else if (dir == way[2])
+	else if (look == way[2])
 	{
 		animation.Play("DieLeft");
 	}
-	else if (dir == way[3])
+	else if (look == way[3])
 	{
 		animation.Play("DieLeftDown");
 	}
-	else if (dir == way[4])
+	else if (look == way[4])
 	{
 		animation.Play("DieDown");
 	}
@@ -325,4 +389,54 @@ void Enemy::OnDie(const sf::Vector2f& dir)
 	isAlive = false;
 	isHanded = false;
 	hand.setTextureRect({ 0, 0, 0, 0 });
+}
+
+void Enemy::OneShot(sf::Vector2f dir, float speed, bool isBlink) // pool반환 필요
+{
+	//SceneGame* scene = (SceneGame*)SCENE_MGR.GetCurrScene();
+	SceneLobby* scene = (SceneLobby*)SCENE_MGR.GetCurrScene();
+	EnemyBullet* bullet = new EnemyBullet();
+	//bullet->Shoot(Utils::Normalize(player->GetPosition() - position), speed);
+	bullet->Shoot(dir, speed);
+	bullet->SetPosition(position);
+	bullet->SetBullet(isBlink);
+	bullet->Init();
+	bullet->Reset();
+	scene->AddGo(bullet);
+
+}
+
+void Enemy::FiveWayShot(sf::Vector2f dir, float speed)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		//dir = Utils::Normalize(player->GetPosition() - position);
+		sf::Vector2f ang =
+		{
+			dir.x * cos(-0.5f + 0.25f * i) - dir.y * sin(-0.5f + 0.25f * i),
+			dir.x * sin(-0.5f + 0.25f * i) + dir.y * cos(-0.5f + 0.25f * i)
+		};
+		OneShot(ang, speed);
+		std::cout << ang.x << " / " << ang.y << std::endl;
+	}
+}
+
+void Enemy::SixWayDie(sf::Vector2f dir, float speed, int chance)
+{
+	if (int ran = Utils::RandomRange(1, 100) <= chance)
+	{
+		OnDie(dir);
+		return;
+	}
+
+	for (int i = 0; i < 6; i++)
+	{
+		OneShot(Utils::DirectionFromAngle(60.f + 60.f * i), speed, true);
+	}
+
+	isAlive = false;
+	isHanded = false;
+	hand.setTextureRect({ 0, 0, 0, 0 });
+
+	SetActive(false);
 }

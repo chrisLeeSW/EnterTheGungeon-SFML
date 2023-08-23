360d04e9164740a443c237b720f4107717748e75
KINjoohyun <gurados@gmail.com>
2023-08-23 23:18:37 +0900

    DisplayItem  - 아이템의 이름과 가격 출력.  - 상호작용 시 None타입으로 변경.  - None타입으로 변경 시 상호작용 불가.  - StringTable 사용.
    리소스 파일 수정
     - Items.png에 판매완료 표시판 추가. (이미지를 찾지 못해 동영상 캡쳐)

--- Enter the Gungeon/PlayerObjects/Item/DisplayItem.cpp
+++ Enter the Gungeon/PlayerObjects/Item/DisplayItem.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 #include "DisplayItem.h"
+#include "DataTableMgr.h"
+#include "StringTable.h"
 
 DisplayItem::DisplayItem(DisplayItem::Names item, const std::string& textureId, const std::string& n)
 	:SpriteGo(textureId, n), item(item)
@@ -14,67 +16,111 @@ void DisplayItem::Init()
 
 void DisplayItem::Reset()
 {
+	StringTable* table = DATATABLE_MGR.Get<StringTable>(DataTable::Ids::String); // StringTable 사용
+	std::string itemname;
+
 	switch (item)
 	{
 	case DisplayItem::Names::None:
-		return;
+		price = 0;
+		sprite.setTexture(*RESOURCE_MGR.GetTexture("graphics/Items.png"));
+		sprite.setTextureRect({ 256, 78, 19, 14 });
+		text.setString("");
+
+		action = []()
+		{
+
+		};
 		break;
 	case DisplayItem::Names::Heart:
 		price = 20;
 		sprite.setTexture(*RESOURCE_MGR.GetTexture("graphics/Items.png"));
 		sprite.setTextureRect({ 1, 13, 13, 13 });
+		itemname = table->Get("HEART") + ": " + std::to_string(price);
+		text.setString(itemname);
+
 		action = [this]()
 		{
 			// if 가격이 안되면 return;
 			// player hp +1
+
+			item = DisplayItem::Names::None;
+			Reset();
 		};
 		break;
 	case DisplayItem::Names::Hearts:
 		price = 30;
 		sprite.setTexture(*RESOURCE_MGR.GetTexture("graphics/Items.png"));
 		sprite.setTextureRect({ 1, 28, 15, 13 });
+		itemname = table->Get("HEARTS") + ": " + std::to_string(price);
+		text.setString(itemname);
+
 		action = [this]()
 		{
 			// if 가격이 안되면 return;
 			// player hp +2
+
+			item = DisplayItem::Names::None;
+			Reset();
 		};
 		break;
 	case DisplayItem::Names::Key:
 		price = 25;
 		sprite.setTexture(*RESOURCE_MGR.GetTexture("graphics/Items.png"));
 		sprite.setTextureRect({ 1, 43, 16, 14 });
+		itemname = table->Get("KEY") + ": " + std::to_string(price);
+		text.setString(itemname);
+
 		action = [this]()
 		{
 			// player key +1
+
+			item = DisplayItem::Names::None;
+			Reset();
 		};
 		break;
 	case DisplayItem::Names::Blank:
 		price = 20;
 		sprite.setTexture(*RESOURCE_MGR.GetTexture("graphics/Items.png"));
 		sprite.setTextureRect({ 1, 61, 12, 12 });
+		itemname = table->Get("BLANK") + ": " + std::to_string(price);
+		text.setString(itemname);
+
 		action = [this]()
 		{
 			// player blank +1
+
+			item = DisplayItem::Names::None;
+			Reset();
 		};
 		break;
 	case DisplayItem::Names::Ammo:
 		price = 30;
 		sprite.setTexture(*RESOURCE_MGR.GetTexture("graphics/Items.png"));
 		sprite.setTextureRect({ 1, 77, 11, 14 });
+		itemname = table->Get("AMMO") + ": " + std::to_string(price);
+		text.setString(itemname);
+
 		action = [this]()
 		{
 			// player ammo 증가 함수
+
+			item = DisplayItem::Names::None;
+			Reset();
 		};
 		break;
 	case DisplayItem::Names::Count:
 		break;
 	default:
+		std::cerr << "ERROR: Not Exist DisplayItem Names" << std::endl;
+		SetActive(false);
+		return;
 		break;
 	}
 
 	interaction = false;
 
-	box.setSize({ 50.f, sprite.getLocalBounds().height });
+	box.setSize({ itemname.size() * 7.f, sprite.getLocalBounds().height });
 	box.setOutlineColor(sf::Color::White);
 	box.setOutlineThickness(1);
 	box.setFillColor(sf::Color::Black);
@@ -82,7 +128,6 @@ void DisplayItem::Reset()
 	text.setCharacterSize(10);
 	text.setFillColor(sf::Color::White);
 	text.setFont(*RESOURCE_MGR.GetFont("fonts/AurulentSansMono-Regular.otf"));
-	text.setString("test");
 
 	SetOrigin(origin);
 
@@ -91,6 +136,8 @@ void DisplayItem::Reset()
 
 void DisplayItem::Update(float dt)
 {
+	if (item == DisplayItem::Names::None) return;
+
 	float distance = Utils::Distance(player->GetPosition(), position);
 	if (distance <= 30.f)
 	{
